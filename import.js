5cd43f8cbc9ea71f31dcfa1db8a219ac5e221d13
Harikrishnan-JNair <124670380+Harikrishnan-JNair@users.noreply.github.com>
2023-02-13 00:56:19 +0530

    Delete import.js

--- import.js
+++
@@ -1,8 +0,0 @@
-
-import * as test from "./calculate.js"
-const principal = test.principal;
-const rate = test.rate;
-const years = test.years;
-console.log("Interest Amount : ", test.calculateRate(principal,rate,years))
-console.log("Total Amount : ", test.calculateTotalAmount(principal,rate,years))
-
